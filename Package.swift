cccd9ed5045fb406a6798e5231719894d706ca02
walker.zhai <walker.zhai@ricepo.com>
2019-08-13 14:39:05 +0800

    upgrade Dollar to version 9.0.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Cent",
     dependencies: [
-        .Package(url: "https://github.com/ankurp/Dollar", majorVersion: 7, minor: 1)
+        .Package(url: "https://github.com/ankurp/Dollar", majorVersion: 9, minor: 0)
     ])
